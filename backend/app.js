042bd8575c1618fc37f7244d57b24db3769e88da
Yogender Rawat <yogenderrawat@example.com>
2025-12-02 00:50:05 +0530

    Added missing index.html

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,14 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 // Connect DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shiftboard', {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(()=> console.log('Connected to MongoDB'))
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error', err));
+
 
 // Models
 const User = require('./models/User');
